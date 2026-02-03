f0172a0c5950200f46a553d6d916646ce6fe1a6d
Ana Regina Múzquiz <anareginarodriguez2006@gmail.com>
2026-02-03 08:42:22 -0600

    Update IPSummary.h

--- IPSummary.h
+++ IPSummary.h
@@ -1,1 +1,15 @@
+#ifndef IP_SUMMARY_H
+#define IP_SUMMARY_H
+
+#include <string>
+
+//almacena el resumen de informacion de una ip
+//contiene el total de accesos salientes y entrantes
+struct IPSummary {
+  std::string ip;
+  int outDegree;
+  int inDegree;
+};
+
+#endif
 
